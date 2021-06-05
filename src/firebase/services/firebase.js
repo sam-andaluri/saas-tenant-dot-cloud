b5160601e3d7522ea23cccf9d62af09dad5e183f
Sambasiva Andaluri <andaluri@gmail.com>
2021-06-05 05:27:22 -0400

    add powerfulseal test

--- src/firebase/services/firebase.js
+++
@@ -1,8 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "../../config";
-
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth;
-export const analytics = firebase.analytics;
